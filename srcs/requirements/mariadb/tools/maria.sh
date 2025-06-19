d0d05c41ffc095ad6306f58c24556c211bf2cd0d
almat101 <sandruozzo@gmail.com>
2025-06-19 17:29:20 +0200

    Add inception part
    Created wordpress,nginx and mariadb containers with alpine base image to ensure lightweight on ec2 instance, TODO php-myadmin container

---
+++ srcs/requirements/mariadb/tools/maria.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+#start mariadbd-safe in the background
+mariadbd-safe &
+
+sleep 5
+# Create the table
+echo "Create the table"
+mariadb -u root -p"${MARIA_ROOT_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS \`${MARIA_DB_NAME}\`;"
+
+# Create the user
+echo "Create the user"
+mariadb -u root -p"${MARIA_ROOT_PASSWORD}" -e "CREATE USER IF NOT EXISTS \`${MARIA_USER}\`@'localhost' IDENTIFIED BY '${MARIA_PASSWORD}';"
+
+# Grant privileges to the user
+echo "Grant privileges to the user"
+mariadb -u root -p"${MARIA_ROOT_PASSWORD}" -e "GRANT ALL PRIVILEGES ON \`${MARIA_DB_NAME}\`.* TO \`${MARIA_USER}\`@'%' IDENTIFIED BY '${MARIA_PASSWORD}';"
+
+#change the root password to the one set in the environment
+echo "Change the root password"
+mariadb -u root -p"${MARIA_ROOT_PASSWORD}" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIA_ROOT_PASSWORD}';"
+
+#flush privileges to apply changes to all users
+echo "Flush privileges"
+mariadb -u root -p${MARIA_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
+
+#shutdown the server
+echo "Shutdown the server"
+mariadb-admin -u root -p"${MARIA_ROOT_PASSWORD}" shutdown
+
+#start the server in safe mode
+echo "Start the server in safe mode"
+exec mariadbd-safe
