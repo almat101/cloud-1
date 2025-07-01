ac7fa18ebbe3ed04436b0ac4a83d14222c0892c5
almat101 <sandruozzo@gmail.com>
2025-07-01 14:26:57 +0200

    feat(nginx): add IP restriction and env toggle for wp-admin
    - Added allow/deny rules to restrict access to /wp-admin and /phpmyadmin to specific IPs (home, school, Docker network)
    - Introduced WP_ADMIN_ACCESSIBLE environment variables to easily enable/disable access to admin panels
    - Enhanced security for WordPress and phpMyAdmin admin areas in production and development environments

--- srcs/requirements/nginx/tools/entrypoint.sh
+++ srcs/requirements/nginx/tools/entrypoint.sh
@@ -2,13 +2,13 @@
 
 set -e                            # Exit immediately if a command exits with a non-zero status
 set -u                            # Treat unset variables as an error and exit immediately
-
-if [ -z "${DOMAIN_NAME}" ] || [ -z "${PMA_ACCESSIBLE}" ]; then   # Check if DOMAIN_NAME or PMA_ACCESSIBLE are unset or empty
-    echo "PLEASE SET DOMAIN_NAME AND PMA_ACCESSIBLE ENVIROMENT VARIABLE"
+set -x      #todo remove for debuggin
+if [ -z "${DOMAIN_NAME}" ] || [ -z "${PMA_ACCESSIBLE}" ] || [ -z "${WP_ADMIN_ACCESSIBLE}" ]; then   # Check if DOMAIN_NAME or PMA_ACCESSIBLE are unset or empty
+    echo "PLEASE SET DOMAIN_NAME PMA_ACCESSIBLE WP_ADMIN_ACCESSIBLE ENVIRONMENT VARIABLE"
     exit 1                      # Exit with error if variables are not set
 fi
 
 echo "Applying envsubst to nginx configuration template"
-envsubst '${DOMAIN_NAME} ${PMA_ACCESSIBLE}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf   # Substitute environment variables in the nginx config template and write to the config file
+envsubst '${DOMAIN_NAME} ${PMA_ACCESSIBLE} ${WP_ADMIN_ACCESSIBLE}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf   # Substitute environment variables in the nginx config template and write to the config file
 
 exec "$@"                        # Replace the shell with the command passed as arguments to the script
